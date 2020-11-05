09acbb8a9745b3028402da3441e16d36516f1b42
Nikita <znv569@gmail.com>
2020-11-05 17:42:28 +0300

    Initial Commit

---
+++ MyTwiter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyTwiter
+//
+//  Created by Admin on 05.11.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
